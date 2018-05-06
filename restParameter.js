9f651b0599b995ac34622355ea5597da665b3130
Brix <brienyll29@gmail.com>
2018-05-06 15:47:08 -0700

    added null argument

--- restParameter.js
+++ restParameter.js
@@ -1,10 +1,13 @@
 function average(...nums) {
+    if (nums.length > 0) {
     let ave = 0;
     for (const num of nums) {
     	ave += num;
     	}
     	return ave / nums.length;
-    	}
+    	} 
+    else return 0;
+}
 console.log(average(2, 6));
 console.log(average(2, 3, 3, 5, 7, 10));
 console.log(average(7, 1432, 12, 13, 100));
